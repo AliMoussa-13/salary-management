259986428a96d92aeb189f040a3d34a5cdf70e0d
Ali Moussa <ali.moussa2004.13>
2025-02-26 10:15:43 -0800

    Editing the the program and seprating loop and add main loop

--- Salary.py
+++ Salary.py
@@ -1,5 +1,8 @@
 import os
 import time
+monthNames=[]
+salaryOfMonths=[]
+
 def StringInputChecker(text):
     number=[0,1,2,3,4,5,6,7,8,9]
     check =False
@@ -23,17 +26,37 @@ while again:
         os.system('cls' if os.name == 'nt' else 'clear')
         print("You entered wrong monthName :(  ")
         print("Enter just the name of the month with any  numbers")
-        continue
-        
-    salaryOfMonth=int(input(f"Enter your salary for {monthName} :"))
+    #    continue  
+    monthNames.append(monthName)
+    
+salaryOfMonth=int(input(f"Enter your salary for {monthName} :"))      
+
+check=True
+while check:
+    check=False
+    
     print("Enter the following perctanges (%) :")
     savingPer=float(input("1)Savings :"))
     rentPer=float(input("2)Rents:"))
     electricityPer=float(input("3)Electricity:"))
     if(savingPer+rentPer+electricityPer>100):
-        again=True
-        os.system('cls' if os.name == 'nt' else 'clear')
+        check=True
+        #os.system('cls' if os.name == 'nt' else 'clear')
         print("The sum of the inputs is greater than 100(%) :( ")
         print("please enter the percentages in correct way in which the sum of the three inputs must be less than 100(%) ")
+    salaryOfMonths.append(salaryOfMonth)
 
-print("hello")
+answer=input("More?")
+
+    
+user ={
+    "Name":userName,
+    "MonthName":monthNames,
+    "Salary":salaryOfMonths
+}
+     
+#i must split the chechking conditions  and loop over the month name and salary to get more than once or put the input 
+# savind and ect above at before the loop and put for it a specific loop
+#check the dictionary methods
+
+
